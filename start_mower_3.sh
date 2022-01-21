776f461a6e0ce4ed28ed82328130e345766e5551
Stryb <32589942+Stryb@users.noreply.github.com>
2022-01-21 11:49:48 +0400

    Update start_mower_3.sh

--- start_mower_3.sh
+++ start_mower_3.sh
@@ -3,6 +3,6 @@ ech0 'bash'
 set -o verbose
 source /opt/kinetic/devel/setup.bash
 source /home/stryb/mower3_ws/devel/setup.bash
-roslaunch mower3_ws start_mw.launch
+roslaunch mower3_diskeyb start_mw.launch
 pwd
 exit 0
